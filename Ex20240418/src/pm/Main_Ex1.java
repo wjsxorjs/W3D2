f35e82d26f939125bb985d98194eac663ce535a5
wjsxorjs <wjsxorjs@naver.com>
2024-04-18 15:56:15 +0900

    2024.04.18 0.7

--- Ex20240418/src/pm/Main_Ex1.java
+++ Ex20240418/src/pm/Main_Ex1.java
@@ -5,6 +5,7 @@ import java.io.Reader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -15,7 +16,10 @@ public class Main_Ex1 {
 
 	SqlSessionFactory factory;
 	
-	
+
+	public Main_Ex1() throws Exception {
+		makeFactory();
+	}
 	
 	private void makeFactory() throws Exception { // - 생성자에서 단 한번만 호출되도록
 		Reader r = Resources.getResourceAsReader("pm/config.xml");
@@ -32,10 +36,7 @@ public class Main_Ex1 {
 		
 		List<MemVO> list = ss.selectList("mem.search_name",m_name);
 		
-		for(int i=0; i<list.size(); i++) {
-			MemVO vo = list.get(i);
-			System.out.printf("%2s | %-6s | %-5s | %-22s | %-15s\r\n",vo.getM_idx(), vo.getM_id(), vo.getM_name(), vo.getM_email(), vo.getM_phone());
-		}
+		printList(list);
 		
 		if(ss != null) {
 			ss.close();
@@ -47,16 +48,26 @@ public class Main_Ex1 {
 		
 		Map<String, String> memap = new HashMap<String, String>();
 		
-		memap.put("m_email", m_email);
-		memap.put("m_phone", m_phone);
 		
+		
+		if(m_email != null) {
+			memap.put("m_email", m_email);
+		}
+		if(m_phone != null) {
+			memap.put("m_phone", m_phone);
+		}
+		
+		
+		if(m_email == null && m_phone == null) {
+			if(ss != null) {
+				ss.close();
+			}
+			return;
+		}
 		
 		List<MemVO> list = ss.selectList("mem.search",memap);
 		
-		for(int i=0; i<list.size(); i++) {
-			MemVO vo = list.get(i);
-			System.out.printf("%2s | %-6s | %-5s | %-22s | %-15s\r\n",vo.getM_idx(), vo.getM_id(), vo.getM_name(), vo.getM_email(), vo.getM_phone());
-		}
+		printList(list);
 		
 		if(ss != null) {
 			ss.close();
@@ -64,20 +75,45 @@ public class Main_Ex1 {
 		
 	}
 	
-	
-	public Main_Ex1() throws Exception {
-		makeFactory();
+	private void printList(List<MemVO> list) {
+		for(int i=0; i<list.size(); i++) {
+			MemVO vo = list.get(i);
+			System.out.printf("%2s | %-6s | %-5s | %-22s | %-15s\r\n",vo.getM_idx(), vo.getM_id(), vo.getM_name(), vo.getM_email(), vo.getM_phone());
+		}
 	}
 	
+		
 	
 	public static void main(String[] args) throws Exception {
 		
 		Main_Ex1 ex1 = new Main_Ex1();
-		System.out.println("----------------------- search_name() -----------------------");
-		ex1.search_name("JA");
+		
+//		Scanner scan = new Scanner(System.in);
+//		
+//		System.out.print("검색할 이름 입력 : ");
+//		String m_name = scan.nextLine();
+//		
+//		System.out.println("----------------------- search_name() -----------------------");
+//		ex1.search_name(m_name);
+//		System.out.println();
+//		
+//		System.out.print("검색할 이메일 입력 : ");
+//		String m_email = scan.nextLine();
+//		System.out.print("같이 검색할 연락처 입력 : ");
+//		String m_phone = scan.nextLine();
+//		System.out.println("-------------------------- search() -------------------------");
+//		ex1.search(m_email, m_phone);
+		
+		ex1.search("ee", null);
 		System.out.println();
-		System.out.println("-------------------------- search() -------------------------");
-		ex1.search("ee","0000");
+		ex1.search(null, "001");
+		System.out.println();
+		ex1.search(null, null);
+		System.out.println();
+		ex1.search("ke", "03");
+		
+		
+		
 	}
 
 }
